10ae0401ed20e599a8a25170a1fa54bb798a06da
Mashanlo <mr.mashanlo@yandex.ru>
2021-07-21 21:53:56 +0600

    add files

--- app/js/app.js
+++ app/js/app.js
@@ -3,15 +3,22 @@
 import $ from 'jquery'; window.jQuery = $; window.$ = $;
 import Swiper from 'swiper';
 
+require('~/app/vendor/animate-svg/animate-svg.js');
 require('~/app/vendor/slidinput/jquery.slidinput.min.js');
 require('~/node_modules/tabslet/jquery.tabslet.min.js');
 require('~/node_modules/@fortawesome/fontawesome-free/js/all.js');
-require('~/app/vendor/animate-svg/animate-svg.js');
+require('~/node_modules/inputmask/dist/jquery.inputmask.min.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	$(window).on('load', function() {
 		
+		$('.phone-mask').inputmask( { mask: "999 999 99 99", placeholder:"" } );
+		$('.card-num-mask').inputmask( { mask: "9999 9999 9999 9999", placeholder:"" } );
+		$('.card-cvc-mask').inputmask( { mask: "999", placeholder:"" } );
+		$('.card-valid-mask').inputmask( { mask: "99 / 9999", placeholder:"" } );
+		$('.card-name-mask').inputmask( { mask: "A{1,15} A{1,15}", placeholder:"" } );
+
 		$('.myInput').slidinput({
 			mode: 'regular',
 			offsetX: 25,
