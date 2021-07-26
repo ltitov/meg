53ace0fb5f667d81fba49df1be21f323317805e9
Mashanlo <mr.mashanlo@yandex.ru>
2021-07-26 21:51:07 +0600

    add slide-menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 import $ from 'jquery'; window.jQuery = $; window.$ = $;
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
+import ionRangeSlider from 'ion-rangeslider';
 
 require('~/app/vendor/animate-svg/animate-svg.js');
 require('~/app/vendor/slidinput/jquery.slidinput.min.js');
@@ -31,8 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		let products = new Swiper(".products", {
 			slidesPerView: 3,
 			spaceBetween: 180,
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
 		});
-
+	
 		let imgSlider = new Swiper(".img-slider", {
 			slidesPerView: 1,
 			allowTouchMove: false,
@@ -41,6 +46,51 @@ document.addEventListener('DOMContentLoaded', () => {
 				clickable: true,
 			},
 		});
+
+		$(".price-form__range").ionRangeSlider({
+			type: "double",
+			min: 0,
+			max: 1000,
+			from: 0,
+			to: 500,
+			grid: false,
+			prefix: "$ ",
+		});
+
+		function select() {
+			let selectHeader = $('.select__header');
+			let selectItem = $('.select__item');
+
+			selectHeader.on( 'click', function() {
+				$(this).parent().toggleClass('is-active');
+			});
+			
+			selectItem.on( 'click', function() {
+				let text = $(this).text();
+				let currentText = $('.select__current');
+				currentText.html(text);
+				$('.select').removeClass('is-active unselected');
+			});
+		};
+		select();
+
+		function slidemenu(menu) {
+			$(`.${menu}`).append(`<div class="${menu}-space slidemenu-space"></div>`).addClass('slidemenu');
+			$(`.${menu}-content`).addClass('slidemenu-content');
+			$(`.${menu}-btn`).addClass(`${menu}-triger`);
+			$(`.${menu}-inner-btn`).addClass(`${menu}-triger`);
+	
+			$(`.${menu}-triger`).on('click', function(e) {
+				e.preventDefault();
+				$(`.${menu}`).toggleClass('is-active');
+			});
+	
+			$(`.${menu}-space`).on('click', function() {
+				$(`.${menu}`).removeClass('is-active');
+			});
+		};
+		slidemenu('dropmenu-filter');
+		slidemenu('dropmenu-cart');
 
 	});
 
